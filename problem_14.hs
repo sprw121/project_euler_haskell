251c7fbd71694eedd28a192d3278524ace34f2f0
Steven Wright <steven@nanosatisfi.com>
2014-06-22 21:43:28 -0700

    Fixed problem 14

--- problem_14.hs
+++ problem_14.hs
@@ -3,5 +3,8 @@ collatz_len acc 1 = acc
 collatz_len acc x = collatz_len (acc+1) y
 	where y = if even x then (quot x 2) else (3*x +1)
 
-max_collatz' n i acc = if i > n then acc else max_collatz' n (i+1) (!max acc (collatz_len 1 i))
+max_collatz' n i acc = if i > n then acc else max_collatz' n (i+1) (max acc (collatz_len 1 i))
 max_collatz n = max_collatz' n 1 0
+
+main = do
+	print $ max_collatz 1000000
